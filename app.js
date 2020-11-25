f0e3ff8a2e893742a1a89e9efe67f3ea891f12f2
Marco <marcoburman@gmail.com>
2020-11-25 17:19:59 +0000

    Create (broken) post route to receive gross salary; start tax logic

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
-const router = require('express').Router()
 const taxRouter = require('./controllers/tax')
 
 app.use('/', taxRouter)
+
+const taxBrackets = [
+  {rate: 0, floor: 0, ceiling: 12500 },
+  { rate: 0.2, floor: 12501, ceiling: 50000 },
+  { rate: 0.4, floor: 50001, ceiling: 150000 },
+  { rate: 0.45, floor: 150001, ceiling: Infinity }
+]
+
+let tax = 0
+
+
 
 module.exports = app
--- controllers/tax.js
+++ controllers/tax.js
@@ -1,7 +1,20 @@
 const taxRouter = require('express').Router()
+const calculator = require('../app')
 
 taxRouter.get('/', (request, response) => {
-  response.send('INCOME TAX!!!')
+  response.send('Income tax calculator')
+})
+
+taxRouter.post('/', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  //const grossSalary = body.grossSalary
+
+  /*if (grossSalary > 10000) {
+    response.send('higher than 10000')
+  }
+  */
 })
 
 module.exports = taxRouter
